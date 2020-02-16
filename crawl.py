d2de19116181890355c682ceacfc7fdbb6f6999d
Lyrs <fleurdelyrs@gmail.com>
2020-02-16 22:09:36 +0100

    crawling script added

---
+++ crawl.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import urllib.request
+from bs4 import BeautifulSoup
+import os
+import time
+
+# TODO: add init (scrap one certain image at the first time)
+# instead of doing in manually, in case of deleting all art files :)
+
+# url address
+URL = "https://mothcub.tumblr.com/tagged/art"
+# page count
+page_no = 1
+# the most recent saved image
+RECENT = sorted(os.listdir("art/pre"))[-1]
+# big img id, should work for the next few years
+img_id = "9"*12
+
+while img_id > RECENT:
+    # crawl source
+    page_source = urllib.request.urlopen(URL+"/page/"+str(page_no))
+    page_html = page_source.read().decode('utf-8')
+    page = BeautifulSoup(page_html, 'html.parser')
+
+    # pretend you're a real person
+    time.sleep(3)
+
+    # scrap the images
+    arts = page.findAll("div", {"class": "photo"})
+    for art in arts:
+        img_id = art.img.parent["href"].split('/')[-1]
+        if img_id == RECENT:
+            break
+        else:
+            urllib.request.urlretrieve(
+                art.img["src"], "art/pre/" + img_id)
+        print(img_id)
+    page_no += 1
